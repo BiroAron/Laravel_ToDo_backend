80148652c911dc29e9ceb00052a7fd4a34c43e67
Biró Áron <biro.aron000802@gmail.com>
2023-09-14 10:29:33 +0300

    ITL 5 - Removed all comments from edited files

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,20 +6,15 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         //
     }
 
-
     public function store(Request $request)
     {
         //
     }
-
 
     public function show(string $user)
     {
